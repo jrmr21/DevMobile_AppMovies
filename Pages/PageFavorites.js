2d884e0edbcf636f0df35c0e2882fa643f02918e
jeremie.robles@mainbot.me <jeremierobles@gmx.fr>
2019-10-31 10:49:06 +0100

    f

--- Pages/PageFavorites.js
+++ Pages/PageFavorites.js
@@ -3,11 +3,7 @@ import {Text,StyleSheet} from 'react-native'
 import { connect } from 'react-redux'
 import FilmList from '../Components/FilmList'
 
-<<<<<<< HEAD:Pages/PageFavorites.js
-class Favorites extends React.Component {
-=======
 class PagesFavorites extends React.Component {
->>>>>>> 1cb3c1406158d58db9744c99f15314f4f32a0e2b:Pages/PageFavorites.js
 
     static navigationOptions = {
         title: 'Mes favoris',
@@ -45,8 +41,4 @@ const mapStateToProps = state => {
     }
   }
 
-<<<<<<< HEAD:Pages/PageFavorites.js
-export default connect(mapStateToProps) (Favorites) 
-=======
 export default connect(mapStateToProps) (PagesFavorites) 
->>>>>>> 1cb3c1406158d58db9744c99f15314f4f32a0e2b:Pages/PageFavorites.js
